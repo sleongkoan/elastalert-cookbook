3a833b76903debf636621832a0391e847c5d966d
Stephen Leong Koan <sleongkoan@gmail.com>
2017-01-16 17:35:52 -0500

    fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,15 +15,15 @@ default['pip-elastalert']['elasticsearch']['alert_time_limit']['value'] = 2
 default['pip-elastalert']['log_dir'] = '/var/log/elastalert'
 
 default['pip-elastalert']['user'] = 'elastalert'
-default['pip-elastalert']['user_home'] = "/home/#{node['elastalert']['user']}"
+default['pip-elastalert']['user_home'] = "/home/#{node['pip-elastalert']['user']}"
 default['pip-elastalert']['group'] = 'elastalert'
 default['pip-elastalert']['directory'] = '/opt/elastalert'
-default['pip-elastalert']['rules_directory'] = "#{node['elastalert']['directory']}/rules"
-default['pip-elastalert']['virtualenv']['directory'] = "#{node['elastalert']['directory']}/.env"
+default['pip-elastalert']['rules_directory'] = "#{node['pip-elastalert']['directory']}/rules"
+default['pip-elastalert']['virtualenv']['directory'] = "#{node['pip-elastalert']['directory']}/.env"
 
-default['pip-elastalert']['supervisor']['logfile'] = "#{node['elastalert']['log_dir']}/elastalert_supervisord.log"
+default['pip-elastalert']['supervisor']['logfile'] = "#{node['pip-elastalert']['log_dir']}/elastalert_supervisord.log"
 default['pip-elastalert']['supervisor']['logfile_maxbytes'] = '1MB'
 default['pip-elastalert']['supervisor']['logfile_backups'] = 2
-default['pip-elastalert']['supervisor']['err_logfile'] = "#{node['elastalert']['log_dir']}/elastalert_stderr.log"
+default['pip-elastalert']['supervisor']['err_logfile'] = "#{node['pip-elastalert']['log_dir']}/elastalert_stderr.log"
 default['pip-elastalert']['supervisor']['err_logfile_maxbytes'] = '5MB'
-default['pip-elastalert']['supervisor']['run_command'] = "#{node['elastalert']['virtualenv']['directory']}/bin/elastalert --config #{node['elastalert']['directory']}/config.yml --verbose"
+default['pip-elastalert']['supervisor']['run_command'] = "#{node['pip-elastalert']['virtualenv']['directory']}/bin/elastalert --config #{node['pip-elastalert']['directory']}/config.yml --verbose"
